c96458345e56cc82b0f7232c3abb68a59fa7c836
ftptpf <Nb49Rtdd>
2022-07-31 20:28:46 +0300

    7. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/persistence/UserDbStore.java
+++ src/main/java/ru/job4j/persistence/UserDbStore.java
@@ -1,13 +1,15 @@
 package ru.job4j.persistence;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.User;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 @Repository
 public class UserDbStore {
@@ -17,31 +19,36 @@ public class UserDbStore {
         this.sf = sf;
     }
 
-    public Optional<User> create(User user) {
-        Session session = sf.openSession();
-        session.beginTransaction();
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
         try {
-            session.save(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Optional.empty();
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
         } finally {
             session.close();
+        }
+    }
+
+    public Optional<User> create(User user) {
+        try {
+            tx(session -> session.save(user));
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return Optional.empty();
         }
         return Optional.of(user);
     }
 
     public List<User> findByLoginAndPassword(String login, String password) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("FROM User WHERE login = :userLogin AND password = :userPassword");
-        query.setParameter("userLogin", login);
-        query.setParameter("userPassword", password);
-        List list = query.list();
-        session.getTransaction().commit();
-        session.close();
-        return list;
+        return tx(session -> session.createQuery("FROM User WHERE login = :userLogin AND password = :userPassword")
+                .setParameter("userLogin", login)
+                .setParameter("userPassword", password)
+                .list());
     }
 
 }
