09f83f3a712d32856a8be6f0b8ec7995820052c4
ftptpf <Nb49Rtdd>
2022-08-07 18:32:55 +0300

    0. ToOne [#6873] - change in sql data

--- db/scripts/tables.sql
+++ db/scripts/tables.sql
@@ -1,14 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY ,
+    name VARCHAR(128) ,
+    login VARCHAR(128) UNIQUE ,
+    password VARCHAR(128)
+);
+
 CREATE TABLE IF NOT EXISTS items (
     id SERIAL PRIMARY KEY ,
     description TEXT ,
     created TIMESTAMP ,
     done BOOLEAN ,
     user_id int NOT NULL REFERENCES users(id)
-);
-
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY ,
-    name VARCHAR(128) ,
-    login VARCHAR(128) UNIQUE ,
-    password VARCHAR(128)
 );
